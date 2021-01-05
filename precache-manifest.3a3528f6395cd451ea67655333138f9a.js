a79306e9f3d714a632a3cb1940dfc6f0091d1873
Aleksey Efimov <factorng@yandex.ru>
2021-01-05 22:11:00 +0300

    Updates

---
+++ precache-manifest.3a3528f6395cd451ea67655333138f9a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "481915f1c9fde78665824cee28ce9c78",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7467215893d95d84bb6d",
+    "url": "./static/css/main.7695e130.chunk.css"
+  },
+  {
+    "revision": "dd127250194d383ace46",
+    "url": "./static/js/2.1421946f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.1421946f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7467215893d95d84bb6d",
+    "url": "./static/js/main.3c4510a8.chunk.js"
+  },
+  {
+    "revision": "5b5af6f035393d4ce943",
+    "url": "./static/js/runtime-main.3199ff88.js"
+  },
+  {
+    "revision": "954ff5c553b72c89dfd37a2646f1b97c",
+    "url": "./static/media/bookmark-black.954ff5c5.svg"
+  },
+  {
+    "revision": "4adfd40981683e3b6be449d062ac949b",
+    "url": "./static/media/bookmark-grey.4adfd409.svg"
+  },
+  {
+    "revision": "979b3ed5d7dc0dbc721d618cccfa0d48",
+    "url": "./static/media/bookmark-marked.979b3ed5.svg"
+  },
+  {
+    "revision": "c2d0901f14de58acae456bda23f80e8a",
+    "url": "./static/media/close_icon.c2d0901f.svg"
+  },
+  {
+    "revision": "6b2028a516009e074f02b37def27b369",
+    "url": "./static/media/main_bg.6b2028a5.png"
+  },
+  {
+    "revision": "71673db715f0bf5e593f93009da99f5c",
+    "url": "./static/media/mobile-menu-black.71673db7.svg"
+  },
+  {
+    "revision": "b4d99d6808f54e9025eea72f5d8dfb2b",
+    "url": "./static/media/mobile-menu-close-white.b4d99d68.svg"
+  },
+  {
+    "revision": "584262ec6706221d6ad0eaed5b9be8a3",
+    "url": "./static/media/mobile-menu-white.584262ec.svg"
+  },
+  {
+    "revision": "c339d7c6d54513f3002516a88aa6faed",
+    "url": "./static/media/my_foto.c339d7c6.jpg"
+  },
+  {
+    "revision": "39d7226d1f440dd7e02dd0c0424e6b43",
+    "url": "./static/media/roboto-black.39d7226d.woff"
+  },
+  {
+    "revision": "6422eac71f54ee5cd007dcf46fc0d22d",
+    "url": "./static/media/roboto-medium.6422eac7.woff"
+  },
+  {
+    "revision": "58bf8e0bd1dd213bf745e9d748caf175",
+    "url": "./static/media/roboto-regular.58bf8e0b.woff"
+  },
+  {
+    "revision": "594bcf8bf5ddbb7302400b1319270585",
+    "url": "./static/media/robotoslab-regular.594bcf8b.woff"
+  },
+  {
+    "revision": "849b68d2aef15cbc97a5e0041b9dd934",
+    "url": "./static/media/trash-black.849b68d2.svg"
+  },
+  {
+    "revision": "5cfec10df32b031fde9b172239cc6d2c",
+    "url": "./static/media/trash-grey.5cfec10d.svg"
+  }
+]);
